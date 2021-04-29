583490987f4fee6228efe2f504c58c68286a3afd
Víctor Guzmán <vguzman.tgz@gmail.com>
2021-04-29 15:49:43 -0500

    Actualización ruta del proyecto

--- Config/Config.php
+++ Config/Config.php
@@ -1,7 +1,7 @@
 <?php
 
 	//define("BASE_URL", "http://localhost/erp-seuat-v1/");
-	const BASE_URL = "http://localhost/erp-seuat-v1";
+	const BASE_URL = "http://localhost/erp-seuat";
 
 	//Zona horaria
 	date_default_timezone_set('America/Mexico_City');
