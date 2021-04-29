123dccf9d9b8a68890834ad71a77586ba91042c4
Víctor Guzmán <vguzman.tgz@gmail.com>
2021-04-29 13:55:01 -0500

    Proyecto inicial con comentarios

---
+++ Assets/js/functions_roles.js
@@ -0,0 +1,40 @@
+var tableRoles;
+
+document.addEventListener('DOMContentLoaded', function(){
+
+	tableRoles = $('#tableRoles').dataTable( {
+		"aProcessing":true,
+		"aServerSide":true,
+        "language": {
+        	//url:"<?php echo media(); ?>/plugins/Spanish.json"
+        	"url": " "+base_url+"/Assets/plugins/Spanish.json"
+        },
+        "ajax":{
+            "url": " "+base_url+"/Roles/getRoles",
+            "dataSrc":""
+        },
+        "columns":[
+            {"data":"id"},
+            {"data":"nombrerol"},
+            {"data":"descripcion"},
+            {"data":"estatus"},
+            {"data":"options"}
+        ],
+        "responsive": true,
+	    "paging": true,
+	    "lengthChange": true,
+	    "searching": true,
+	    "ordering": true,
+	    "info": true,
+	    "autoWidth": false,
+	    "scrollY": '42vh',
+	    "scrollCollapse": true,
+	    "bDestroy": true,
+	    "order": [[ 0, "desc" ]],
+	    "iDisplayLength": 25
+    });
+
+
+});
+
+$('#tableRoles').DataTable();
