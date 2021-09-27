e8faa334e16bdb010312983d13fc3d44dcaad19a
Víctor Guzmán <vguzman.tgz@gmail.com>
2021-09-27 10:49:56 -0500

    Creación vista login y se agregaron Controladores, Modelos y funciones de Login y Logout

--- Controllers/Dashboard.php
+++ Controllers/Dashboard.php
@@ -3,6 +3,12 @@
 		public function __construct()
 		{
 			parent::__construct();
+
+			session_start();
+			if(empty($_SESSION['login']))
+			{
+				header('Location: '.base_url().'/login');
+			}
 		}
 
 		public function Dashboard()
